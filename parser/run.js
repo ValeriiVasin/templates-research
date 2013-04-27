c65241a88b8c7d4def33aa0b30b53b2fb5387285
Valeriy Vasin <valeriy.vasin@sup.com>
2013-04-27 15:38:27 +0400

    Show compilation errors.

--- parser/run.js
+++ parser/run.js
@@ -1,8 +1,18 @@
 var fs = require('fs'),
     Tag = require('./parser_underscore'),
-    file = process.argv[2];
+    file = process.argv[2],
+    errorMessage = null;
 
-fs.writeFileSync(
-    file.replace('.tmpl', '.underscore.tmpl'),
-    Tag.parseFile(file)
-);
+try {
+    fs.writeFileSync(
+        file.replace('.tmpl', '.underscore.tmpl'),
+        Tag.parseFile(file)
+    );
+} catch (error) {
+    errorMessage = error.message;
+} finally {
+    if (errorMessage) {
+        console.log('FAIL %s', file);
+        console.log(errorMessage);
+    }
+}
